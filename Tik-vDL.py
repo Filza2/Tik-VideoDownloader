e0d8ba823ec5fa4e4bea7e7d0f0a62effcb68dcd
TweakPY <69265406+Filza2@users.noreply.github.com>
2022-07-25 14:47:17 +0300

    Update Tik-vDL.py

--- Tik-vDL.py
+++ Tik-vDL.py
@@ -1,5 +1,6 @@
 from requests import get
 from time import sleep
+#YOU MUST READ "README" FILE BEFORE RUN THE TOOL
 print("""
 ████████╗██╗██╗  ██╗     ██╗   ██╗██████╗ ██╗     
 ╚══██╔══╝██║██║ ██╔╝     ██║   ██║██╔══██╗██║     
@@ -16,7 +17,12 @@ if vaild_v=='vt':user_vedid=user_vedid.replace('vt','vm')
 elif 'https://www.tiktok.com/@' in user_vedid:
     try:
         ved_id_skip=user_vedid.split('/video/')[1].split('?is_from_webapp')[0]
-        r2=get(f"https://tiktok.livecounts.io/videoDownload/{ved_id_skip}",headers={'Host': 'tiktok.livecounts.io','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': '*/*','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://tokcounter.com/','X-Midas': '1a076be7ac3949de4487f8c8deb29946f5e4537037617eb75ccc6fb3975d98c2','X-Aurora': '4976010164757','X-Mayhem': 'TweakPY-vv1ck','Origin': 'https://tokcounter.com','Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'cross-site','Te': 'trailers'})
+        r2=get(f"https://tiktok.livecounts.io/videoDownload/{ved_id_skip}",headers={'Host': 'tiktok.livecounts.io','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': '*/*','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://tokcounter.com/',
+'X-Midas': '6d39063decda45d796d043e3c5570884b83a64072df6c66ae97bc6471be38554',
+'X-Aurora': '4976244355026',
+'X-Maven': '76f088c63041b7b049b3172220ef741c4a258f069295e62d30d29da585a92900',
+'X-Joey': '1658748118342',
+'X-Mayhem': '553246736447566b58312f697330687930634247756675374f51726c724c426d7a4e51785051484c7176596357674346474c6e6d39667957344547554e785753','Origin': 'https://tokcounter.com','Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'cross-site','Te': 'trailers'})
         print('[+] Done ..')
         sleep(1.4)
         ved_title=r2.json()['video']['title'];ved_downloadurl=r2.json()['video']['downloadUrl']
@@ -46,14 +52,24 @@ elif 'https://www.tiktok.com/@' in user_vedid:
     except Exception as i:exit(f'[!] Failed to fetch download link : {i}')
 elif vaild_v=='vm':pass
 else:print('[!] Sorry This Type of urls not supported now but the tool will try to run anyway')
-r1=get(f'https://api.tokcount.com/?type=videoId&username={user_vedid})',headers={'Host': 'api.tokcount.com','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': '*/*','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://tokcounter.com/','X-Midas': '1a076be7ac3949de4487f8c8deb29946f5e4537037617eb75ccc6fb3975d98c2','X-Aurora': '4976010164757','X-Mayhem': 'TweakPY-vv1ck','Origin': 'https://tokcounter.com','Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'cross-site','Te': 'trailers'})
+r1=get(f'https://api.tokcount.com/?type=videoId&username={user_vedid})',headers={'Host': 'api.tokcount.com','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': '*/*','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://tokcounter.com/',
+'X-Midas': '6d39063decda45d796d043e3c5570884b83a64072df6c66ae97bc6471be38554',
+'X-Aurora': '4976244355026',
+'X-Maven': '76f088c63041b7b049b3172220ef741c4a258f069295e62d30d29da585a92900',
+'X-Joey': '1658748118342',
+'X-Mayhem': '553246736447566b58312f697330687930634247756675374f51726c724c426d7a4e51785051484c7176596357674346474c6e6d39667957344547554e785753','Origin': 'https://tokcounter.com','Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'cross-site','Te': 'trailers'})
 if r1.status_code==500:exit("[!] Video url is invalid or Video Not Found !")
 elif r1.json()["success"]==False:exit("[!] Video url is invalid or Video Not Found !")
 try:
     ved_id=r1.json()['id']
     print("[-] video id Found ..")
     sleep(1.5)
-    r2=get(f"https://tiktok.livecounts.io/videoDownload/{ved_id}",headers={'Host': 'tiktok.livecounts.io','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': '*/*','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://tokcounter.com/','X-Midas': '1a076be7ac3949de4487f8c8deb29946f5e4537037617eb75ccc6fb3975d98c2','X-Aurora': '4976010164757','X-Mayhem': 'TweakPY-vv1ck','Origin': 'https://tokcounter.com','Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'cross-site','Te': 'trailers'})
+    r2=get(f"https://tiktok.livecounts.io/videoDownload/{ved_id}",headers={'Host': 'tiktok.livecounts.io','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': '*/*','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://tokcounter.com/',
+'X-Midas': '6d39063decda45d796d043e3c5570884b83a64072df6c66ae97bc6471be38554',
+'X-Aurora': '4976244355026',
+'X-Maven': '76f088c63041b7b049b3172220ef741c4a258f069295e62d30d29da585a92900',
+'X-Joey': '1658748118342',
+'X-Mayhem': '553246736447566b58312f697330687930634247756675374f51726c724c426d7a4e51785051484c7176596357674346474c6e6d39667957344547554e785753','Origin': 'https://tokcounter.com','Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'cross-site','Te': 'trailers'})
     print('[+] Done ..')
     sleep(1.4)
     ved_title=r2.json()['video']['title'];ved_downloadurl=r2.json()['video']['downloadUrl']
